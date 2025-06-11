9cd87455cce4dd68b6ba2d5f061ceb2b34fea8f9
jorozcove <jorozcove@unal.edu.co>
2025-06-11 01:53:30 -0500

    Attemp to made AIs inmune to their own traps

--- decompile_gamemodes/General/231/231_006_RB_Hazard_HurtDriver.c
+++ decompile_gamemodes/General/231/231_006_RB_Hazard_HurtDriver.c
@@ -1,30 +1,39 @@
 #include <common.h>
+#include "../../../mods/game_modes/Gamemode_Selection/src/utils.h"
 
 int DECOMP_RB_Hazard_HurtDriver(
-	struct Driver* driverVictim, 
-	int damageType,
-	struct Driver* driverAttacker,
-	int reason)
+    struct Driver* driverVictim, 
+    int damageType,
+    struct Driver* driverAttacker,
+    int reason)
 {
-	struct GameTracker* gGT = sdata->gGT;
-	int result=0;
+    struct GameTracker* gGT = sdata->gGT;
+    int result=0;
+    
+    // Check if boss challenge is active and if AI driver is hitting themselves
+    // if (USE_BOSS_CHALLENGE && 
+    //     driverAttacker == driverVictim && 
+    //     (driverVictim->actionsFlagSet & 0x100000) != 0) {
+    //     // AI is immune to their own items in boss challenge mode
+    //     return 0;
+    // }
 
-	if ((driverVictim->actionsFlagSet & 0x100000) == 0)
-	{
-		#ifndef REBUILD_PS1 //this ifndef should be removed at some point?
-		result = VehPickState_NewState(driverVictim, damageType, driverAttacker, reason);
-		#endif
-	}
-	else
-	{
-		//decomp attempt 1 had the second condition of this if statement as: ((gGT->gameMode1 & ADVENTURE_BOSS) != 0)
-		//currently using ((int)gGT->gameMode1 < 0) bc that's what ghidra says (it might be equivalent idk)
-		if ((gGT->levelID == OXIDE_STATION) && (gGT->gameMode1 < 0))
-			damageType = 1;
+    if ((driverVictim->actionsFlagSet & 0x100000) == 0)
+    {
+        #ifndef REBUILD_PS1 //this ifndef should be removed at some point?
+        result = VehPickState_NewState(driverVictim, damageType, driverAttacker, reason);
+        #endif
+    }
+    else
+    {
+        //decomp attempt 1 had the second condition of this if statement as: ((gGT->gameMode1 & ADVENTURE_BOSS) != 0)
+        //currently using ((int)gGT->gameMode1 < 0) bc that's what ghidra says (it might be equivalent idk)
+        if ((gGT->levelID == OXIDE_STATION) && (gGT->gameMode1 < 0))
+            damageType = 1;
 
-		#ifndef REBUILD_PS1 //this ifndef should be removed at some point?
-		result = (int)BOTS_ChangeState(driverVictim, damageType, driverAttacker, reason);
-		#endif
-	}
-	return result;
+        #ifndef REBUILD_PS1 //this ifndef should be removed at some point?
+        result = (int)BOTS_ChangeState(driverVictim, damageType, driverAttacker, reason);
+        #endif
+    }
+    return result;
 }
