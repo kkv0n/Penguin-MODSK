a2e7e4a98bf2aa3674aab3039f8152b3ac74a95e
jorozcove <jorozcove@unal.edu.co>
2025-09-05 21:14:00 -0500

    moon gravity small refactor

---
+++ decompile/General/AltMods/Gasmoxian/GameModes/stats_modifiers.c
@@ -0,0 +1,29 @@
+#include "common.h"
+#include "../utils.h"
+
+// Gravity: 0
+void SetGravity(short gravity) { 
+	for (int i = 0; i <= 4; i++) {
+		data.metaPhys[0].value[i] = gravity; 
+	}
+}
+
+// Speed: 11
+void SetSpeed(int speed) { 
+	for (int i = 0; i <= 4; i++) {
+		data.metaPhys[11].value[i] = speed; 
+	}
+}
+
+void ApplyModifiers(){
+	struct GameTracker *gGT = sdata->gGT;
+	for (unsigned char i = 0; i < (gGT->numPlyrCurrGame + gGT->numBotsNextGame); i++) {
+		struct Driver* driver = gGT->drivers[i];
+
+		if (driver == NULL) continue;
+		// if (gGT->levelID > CITADEL_CITY) continue;
+
+		VehBirth_SetConsts(driver);
+	}
+}
+
