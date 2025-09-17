0a56f687428b13b5a23186c5ef5bb6907bb81b87
jorozcove <jorozcove@unal.edu.co>
2025-09-16 20:41:03 -0500

    Adjustements to player over head names

--- decompile/General/AltMods/Gasmoxian/drawoverheadnames.c
+++ decompile/General/AltMods/Gasmoxian/drawoverheadnames.c
@@ -6,58 +6,56 @@ extern int GetOverheadLen(struct Driver* d);
 
 void DrawOverheadNames()
 {
-	int i;
-	MATRIX* m;
+    int i;
+    MATRIX* m;
 
-	struct GameTracker* gGT = sdata->gGT;
-	struct MyData* ptrDest = (struct MyData*)0x1f800108;
+    struct GameTracker* gGT = sdata->gGT;
+    struct MyData* ptrDest = (struct MyData*)0x1f800108;
 
-	// pushBuffer offset 0x28
-	m = &gGT->pushBuffer[0].matrix_ViewProj;
+    // pushBuffer offset 0x28
+    m = &gGT->pushBuffer[0].matrix_ViewProj;
     gte_SetRotMatrix(m);
     gte_SetTransMatrix(m);
 
-	#if 0
-	DrawOverheadCalibration();
-	#endif
+    // start from P2
+    i = 1;
 
-	// start from P2
-	i = 1;
-	int color = (JUSTIFY_CENTER | ORANGE);
+    if((gGT->gameMode1 & START_OF_RACE) != 0)
+    {
+        // start from P1
+        i = 0;
+    }
 
-	if((gGT->gameMode1 & START_OF_RACE) != 0)
-	{
-		// start from P1
-		i = 0;
-		color = (JUSTIFY_CENTER | OXIDE_LIGHT_GREEN);
+    for(i; i < octr->NumDrivers; i++)
+    {
+        int len = GetOverheadLen(gGT->drivers[i]);
+        if(len == 0) continue;
 
-	}
+        // if mirror mode
+        if(USE_MIRROR)
+        {
+            ptrDest->Screen_posX = 0x200 - ptrDest->Screen_posX;
+        }
 
-	for(i; i < octr->NumDrivers; i++)
-	{
-		int len = GetOverheadLen(gGT->drivers[i]);
-		if(len == 0) continue;
-
-//mirror mode enabled
-
-
-		// if mirror mode
-		if(USE_MIRROR)
-		{
-			ptrDest->Screen_posX =
-			0x200 - ptrDest->Screen_posX;
-		}
-
-
-
-		DECOMP_DecalFont_DrawLineStrlen(
-			octr->nameBuffer[i],
-			len,
-			ptrDest->Screen_posX,
-			ptrDest->Screen_posY-0x4,
-			FONT_SMALL, color);
-
-		// all players except P1
-		color = (JUSTIFY_CENTER | ORANGE);
-	}
+        // Calculate color based on character ID
+        int color = JUSTIFY_CENTER;
+        if(i == 0)
+        {
+            // Player 1 gets special color
+            color |= OXIDE_LIGHT_GREEN;
+        }
+        else
+        {
+            // Use character-based color for other players
+            // color |= (data.characterIDs[i] + 5);
+			color |= ORANGE;
+        }
+        
+        DECOMP_DecalFont_DrawLineStrlen(
+            octr->nameBuffer[i],
+            len,
+            ptrDest->Screen_posX,
+            ptrDest->Screen_posY-0x4,
+            FONT_SMALL, color);
+    }
 }
--- decompile/General/AltMods/Gasmoxian/getoverheadlen.c
+++ decompile/General/AltMods/Gasmoxian/getoverheadlen.c
@@ -1,5 +1,6 @@
 #include <common.h>
 #include "global.h"
+#include "utils.h"
 #include "namesdata.c"
 
 
@@ -7,7 +8,7 @@ int GetOverheadLen(struct Driver* d)
 {
 	struct MyData* ptrDest = (struct MyData*)0x1f800108;
 	ptrDest->World_posX = d->posCurr.x >> 8;
-	ptrDest->World_posY = (d->posCurr.y >> 8) + 25;
+	ptrDest->World_posY = (d->posCurr.y >> 8) + 75;
 	ptrDest->World_posZ = d->posCurr.z >> 8;
 	ptrDest->World_posW = 0;
 
@@ -19,11 +20,19 @@ int GetOverheadLen(struct Driver* d)
 
 	int posZ = ptrDest->Screen_posZ;
 
-	if(posZ < 150) return 0;
-	if(posZ < 190) return 6;
-	if(posZ < 234) return 5;
-	if(posZ < 331) return 4;
-	if(posZ < 475) return 3;
-	if(posZ < 906) return 2;
+	if(!USE_ITEMLESS){
+		if(posZ < 150) return 0;
+		if(posZ < 2700) return 9;
+	}
+
+	else{
+		if(posZ < 150) return 0;
+		if(posZ < 190) return 6;
+		if(posZ < 234) return 5;
+		if(posZ < 331) return 4;
+		if(posZ < 475) return 3;
+		if(posZ < 906) return 2;
+	}
+
 	return 0;
 }
