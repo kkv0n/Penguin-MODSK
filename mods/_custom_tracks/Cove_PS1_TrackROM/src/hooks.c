b41da466d86f2e9c1022305a12d492e155b3fcc2
penta3 <thedragopenta9@gmail.com>
2025-10-10 11:32:19 -0300

    add lod fix for cove rom

--- mods/_custom_tracks/Cove_PS1_TrackROM/src/hooks.c
+++ mods/_custom_tracks/Cove_PS1_TrackROM/src/hooks.c
@@ -266,6 +266,10 @@ void RunUpdateHook()
 {
 	struct GameTracker* gGT = sdata->gGT;
 	
+	//high lod fix
+	*(unsigned int*)0x800AB460 = 0x800A6F40;
+
+	
 		if ((gGT->numPlyrCurrGame == 1) && ((gGT->gameMode1 & END_OF_RACE) == 0))
 		{
 	DrawReservesMeter(data.hud_1P_P1[0x8].x, data.hud_1P_P1[0x8].y + 5, (struct Driver*)sdata->gGT->threadBuckets[0].thread->object);
